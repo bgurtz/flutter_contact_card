c81e66cd4c1bb9a0da10519631f6dd1e03f7751d
Brian Gurtz <bgurtz@gmail.com>
2020-05-03 18:38:21 -0500

    Column Layout Example

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,12 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueGrey[600],
         body: SafeArea(
           child: Column(
+            // mainAxisSize: MainAxisSize.min, // will shrink the size of the column to the min amount needded
+            // mainAxisAlignment: MainAxisAlignment.end, // will move the three boxes to the bottom of the screen,
+            // mainAxisAlignment: MainAxisAlignment.center, // This will move our boxes to the middle of the screen
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // will space the boxes evenly 
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween, // this will put a box at the top, one at the bottom, & one in the middle
+            // mainAxisAlignment: MainAxisAlignment.spaceAround, // this is just a bit diffrent then space evenly
             children: <Widget>[
               Container(
                 height: 100,
@@ -25,6 +31,8 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 child: Text('Container 1'),
               ),
+            // The sizedBox adds a little space between our boxes  
+              SizedBox(height: 20.0),
               Container(
                 width: 100.0,
                 height: 100.0,
