941227c250fa7041a2c7ab7be05ce135fd07837d
Brian Gurtz <bgurtz@gmail.com>
2020-05-03 21:01:51 -0500

    Circle Avatar & Custom Font

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
   );
 }
 
-// Column Layout Example: copy this and run the code to see how to build a basic column's
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
@@ -17,34 +16,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey[600],
         body: SafeArea(
-          child: Row(
-            // mainAxisSize: MainAxisSize.min, // will shrink the size of the column to the min amount needded
-            // mainAxisAlignment: MainAxisAlignment.end, // will move the three boxes to the bottom of the screen,
-            // mainAxisAlignment: MainAxisAlignment.center, // This will move our boxes to the middle of the screen
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // will space the boxes evenly 
-             mainAxisAlignment: MainAxisAlignment.spaceBetween, // this will put a box at the top, one at the bottom, & one in the middle
-            // mainAxisAlignment: MainAxisAlignment.spaceAround, // this is just a bit diffrent then space evenly
+          child: Column(
             children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: 100,
+              
+              CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/images/brian.jpg'),
+                ),
+              ),
+              Text("Brian Gurtz 1",
+              style: TextStyle(
+                fontFamily: 'DancingScript',
+                fontSize:(40.0),
                 color: Colors.white,
-                child: Text('Container 1'),
               ),
-            // The sizedBox adds a little space between our boxes  
-            // SizedBox(height: 20.0),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blueAccent,
-                child: Text('Container 2'),
-              ),
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.redAccent,
-                child: Text('Container 3'),
-              ),
+             ),
             ],
           ),
         ),
